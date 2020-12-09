1e51048843688f7c8c8eb88c8e51d56bddd18edd
Aboyeji, Solomon Adeleke <solesty7@gmail.com>
2020-12-09 08:11:04 +0100

    Two server calls

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,31 @@
 import Link from 'next/link'
 
-function Index({ stars }) {
+function Index({ stars, thejson }) {
   return (
     <div>
       <p>Next.js has {stars} ⭐️</p>
       <Link href="/preact-stars">
         <a>How about preact?</a>
       </Link>
+      {
+        thejson.completed ? <p>{thejson.title}</p> : <p>JSON Place holder task not completed.</p>
+      }
     </div>
   )
 }
 
 export async function getStaticProps() {
-  setTimeout(() => {
-    console.log("just waiting")
-  }, 60 * 1000);
   const res = await fetch('https://api.github.com/repos/vercel/next.js')
   const json = await res.json()
+
+  const jsonres = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+  const thejson = await jsonres.json()
 
   return {
     props: {
       stars: json.stargazers_count,
+      thejson: thejson
+
     },
   }
 }
